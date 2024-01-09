442afe76f5b11a78539d0efd9e5425d76f1fd191
SkorikLL <skorik555web@gmail.com>
2024-01-09 20:47:16 +0700

    add 2.15 Modular JS

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //import { renderLogin } from "./loginPage.js";
-import { getTodos, postTodos } from "/api.js";
+import { fetchPromiseGet, renderUsers } from "./userPage.js";
+import { postTodos } from "/api.js";
 
 // Код писать здесь
 const nameInputElement = document.querySelector(".add-form-name"); // пользователь вводить свое имя
@@ -9,10 +10,7 @@ const listCommentsElements = document.querySelector(".comments");
 const buttonDel = document.querySelector(".del-form-button"); //кнопка "Удалить последний комментарий"
 const loadingTextElement = document.querySelector(".loadingText");
 const loadingTextFormElement = document.querySelector(".loadingTextForm");
-//Объект
-let users = [];
 
-const date = new Date();
 //При первом запуске показывает сообщение "Пожалуйста подождите, загружаю комментарии.....", после того как наши комментарии загрузятся сообщение удаляется.
 function fetchPromiseGetStart() {
   loadingTextElement.textContent =
@@ -23,124 +21,6 @@ function fetchPromiseGetStart() {
 }
 
 fetchPromiseGetStart();
-
-//Делаем запрос в сервер API метод GET
-function fetchPromiseGet() {
-  return getTodos().then((responseData) => {
-    const appComments = responseData.comments.map((comment) => {
-      return {
-        name: comment.author.name,
-        id: comment.id,
-        time: new Date(comment.date),
-        comment: comment.text,
-        quantityLike: comment.likes,
-        isLike: false,
-      };
-    });
-    users = appComments;
-    renderUsers();
-  });
-}
-
-// function fetchPromiseDel() {
-//   return deleteTodos().then(() => {
-//     return fetchPromiseGet();
-//   });
-//   renderUsers();
-// }
-
-export const renderUsers = () => {
-  //const appElement = document.getElementById("app");
-
-  const userHTML = users
-    .map((user, index) => {
-      return `<li data-index="${index}" class="comment">
-        <div class="comment-header">
-          <div>${user.name}</div>
-          <div>${user.time.getDate()}.${
-        user.time.getMonth() + 1
-      }.${user.time.toLocaleDateString("ru-RU", {
-        year: "2-digit",
-      })} ${user.time.getHours()}:${user.time.getMinutes()}</div>
-        </div>
-        <div class="comment-body">
-          <div class="comment-text">
-            ${user.comment}
-          </div>
-        </div>
-        <div class="comment-footer">
-          <div class="likes">
-            <span data-index="${index}" class="likes-counter">${
-        user.quantityLike
-      }</span>
-            <button data-index="${index}" class="like-button ${
-        user.isLike ? "-active-like" : ""
-      }"></button>
-          </div>
-        </div>
-      </li>`;
-    })
-    .join("");
-
-  //   const appHtml = `
-  //       <div class="container">
-  //     <span class="loadingText"></span>
-  //     <ul class="comments">${userHTML}
-  //       //список рендерится из JS
-  //     </ul>
-  //     <span class="loadingTextForm"></span>
-  //     <div class="add-form">
-
-  //       <input type="text" class="add-form-name" placeholder="Введите ваше имя" />
-  //       <textarea type="textarea" class="add-form-text" placeholder="Введите ваш комментарий" rows="4"></textarea>
-  //       <div class="add-form-row">
-  //         <button class="add-form-button">Написать</button>
-  //         <button class="del-form-button">Удалить последний комментарий</button>
-  //       </div>
-  //     </div>
-  //   </div>
-
-  // `;
-  //appElement.innerHTML = appHtml;
-  listCommentsElements.innerHTML = userHTML;
-
-  userClickComment();
-  userClickLike();
-};
-
-renderUsers();
-
-//Добавить обработку клика значка "Like"
-
-function userClickLike() {
-  const allButtonLikes = document.querySelectorAll(".like-button");
-  for (const buttonLike of allButtonLikes) {
-    buttonLike.addEventListener("click", (event) => {
-      event.stopPropagation(); //останавливает всплытие события вверх по дереву.
-      const index = buttonLike.dataset.index;
-      if (users[index].isLike === false) {
-        users[index].quantityLike++;
-        users[index].isLike = true;
-      } else {
-        users[index].quantityLike--;
-        users[index].isLike = false;
-      }
-      renderUsers();
-    });
-  }
-}
-
-//Добавить обработку клика значка "Комментарий" (Пользовать сможет ответить на комментарий)
-function userClickComment() {
-  const allComments = document.querySelectorAll(".comment");
-  for (const comment of allComments) {
-    comment.addEventListener("click", () => {
-      const index = comment.dataset.index;
-      const textElementContent = users[index].comment;
-      commentInputElement.value = `> ${textElementContent}\n ${users[index].name}\n`;
-    });
-  }
-}
 
 //Добавить обработку клика кнопки "Написать"
 buttonElement.addEventListener("click", () => {
